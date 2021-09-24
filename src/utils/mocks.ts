d211d9705d9ee0571ddf244cf0c49737f5edffb5
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-09-24 11:59:54 +0200

    refactor: update mock data for test

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -1,39 +1,35 @@
-import { Cart, Category, Customer, Product } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 import * as Factory from 'factory.ts'
 import { address, commerce, datatype, date, internet, name } from 'faker'
 
-export const productFactory = Factory.Sync.makeFactory<Product>({
-  id: `${Factory.each((i) => i)}`,
-  name: commerce.productName(),
-  price: 2500,
-  description: commerce.productDescription(),
-  expiresAt: date.future(),
-  sku: datatype.number(),
-  stockLevel: 20,
-  categoryId: `${Factory.each((i) => i)}`,
-})
+export const categoryFactory =
+  Factory.Sync.makeFactory<Prisma.CategoryCreateManyInput>({
+    name: commerce.department(),
+  })
 
-export const categoryFactory = Factory.Sync.makeFactory<Category>({
-  id: `${Factory.each((i) => i)}`,
-  name: 'Food',
-})
+export const productFactory =
+  Factory.Sync.makeFactory<Prisma.ProductCreateManyInput>({
+    name: commerce.productName(),
+    price: 2500,
+    description: commerce.productDescription(),
+    expiresAt: date.future(),
+    sku: datatype.number(),
+    stockLevel: 20,
+    categoryId: 1,
+  })
 
-export const cartFactory = Factory.Sync.makeFactory<Cart>({
-  id: `${Factory.each((i) => i)}`,
-  customerId: `1`,
-  isCheckedOut: false,
-  createdAt: Factory.Sync.each(() => new Date()),
-  updatedAt: Factory.Sync.each(() => new Date()),
-})
+export const cartFactory = Factory.Sync.makeFactory<Prisma.CartCreateManyInput>(
+  {
+    customerId: 1,
+    isCheckedOut: false,
+    createdAt: Factory.Sync.each(() => new Date()),
+    updatedAt: Factory.Sync.each(() => new Date()),
+  },
+)
 
-export const cartItemFactory = Factory.Sync.makeFactory<Category>({
-  id: `${Factory.each((i) => i)}`,
-  name: 'Food',
-})
-
-export const customerFactory = Factory.Sync.makeFactory<Customer>({
-  id: `${Factory.each((i) => i)}`,
-  email: internet.email(),
-  name: name.findName(),
-  address: address.streetAddress(),
-})
+export const customerFactory =
+  Factory.Sync.makeFactory<Prisma.CustomerCreateManyInput>({
+    email: Factory.Sync.each(() => internet.exampleEmail()),
+    name: name.findName(),
+    address: address.streetAddress(),
+  })
