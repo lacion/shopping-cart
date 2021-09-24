8ff83670762e5ed720a0c0360a8f4fa31aea0674
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-09-24 12:14:08 +0200

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 30000,
+  testPathIgnorePatterns: [
+    '<rootDir>/src/schema/resolvers/Cart/__tests__/e2e.test.ts',
+  ],
 }
