45d7a9a175c61747dbff3648c9cb89cfccfb2bd9
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-09-28 11:59:01 +0200

    chore: cleanup jest config

--- jest.ci.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testTimeout: 30000,
-  testPathIgnorePatterns: [
-    '<rootDir>/src/schema/resolvers/Cart/__tests__/e2e.test.ts',
-  ],
-}
