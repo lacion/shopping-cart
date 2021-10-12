e2b10a9a45df6edf521ae8e84186e3082b28843d
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-10-12 20:50:14 +0200

    chore: update imports

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 30000,
-  // testPathIgnorePatterns: [
-  //   '<rootDir>/src/schema/resolvers/Cart/__tests__/e2e.test.ts',
-  // ],
+  moduleNameMapper: {
+    'src/utils': '<rootDir>/src/utils',
+    'src/generated': '<rootDir>/src/generated',
+  },
 }
